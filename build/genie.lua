df8d8fd66846235695bb96aada3911416504df0e
nsweb <nsweb@free.fr>
2022-01-08 14:49:29 +0100

    improve build script

--- build/genie.lua
+++ build/genie.lua
@@ -1,5 +1,5 @@
 
-function SetTarget( _configuration, _platform, _basepath )
+function setBigfxTarget( _configuration, _platform, _basepath )
 	local strtarget = string.format( "%s/bin/%s_%s/", _basepath, _configuration, _platform ) 
 	local strobj = string.format( "%s/intermediate/%s_%s", _basepath, _configuration, _platform ) 
 	configuration {_configuration, _platform}
@@ -7,7 +7,7 @@ function SetTarget( _configuration, _platform, _basepath )
 		objdir( strobj )
 end
 
-function SetLibs( _configuration, _platform, _basepath, _enginepath )
+function setBigfxLibs( _configuration, _platform, _bigfxpath )
 	local platformname = _platform
 	if _platform == "x64" then
 		platformname = "Win64"
@@ -23,66 +23,78 @@ function SetLibs( _configuration, _platform, _basepath, _enginepath )
 	-- local sdl2path = string.format( "%s/3rdparty/SDL2-2.0.16/lib/%s", _basepath, _platform)
 	-- local sdl2options = string.format( "-F %s/3rdparty/SDL2-2.0.16/lib/%s", _basepath, _platform)
 
-    local bgfxpath = string.format( "%s/.build/%s_%s/bin", _enginepath, platformname, _ACTION)
+    local bgfxpath = string.format( "%s/3rdparty/bgfx/.build/%s_%s/bin", _bigfxpath, platformname, _ACTION)
 	configuration {_configuration, _platform}
 		--libdirs { glewpath, sdl2path, bgfxpath }
         libdirs { bgfxpath }
 end
 
+function createBigfxProject(_bigfxpath)
+    project "bigfx"
 
---------------------------------------------------------
-solution "bigfx"
-	configurations { "Debug", "Release" }
+    kind "StaticLib"
+    language "C++"
 
-	platforms { "x64", "native" }
+    files { _bigfxpath .. "/src/**.h", _bigfxpath .. "/src/**.cpp" }
 
-    ---------------------------------------------------------
+    local thirdpartypath = _bigfxpath .. "/3rdparty"
+    local bxpath = thirdpartypath .. "/bx"
+    includedirs {
+                --PROJ_DIR .. "/3rdparty/SDL2-2.0.16/include",
+                --PROJ_DIR .. "/3rdparty/glew-2.1.0/include",
+                thirdpartypath .. "/zlib-1.2.11",
+                thirdpartypath .. "/jsmn",
+                --PROJ_DIR .. "/3rdparty/imgui",
+                bxpath .. "/include",
+                bxpath .. "/include/compat/msvc",
+                thirdpartypath }
+
+    defines { "_CRT_SECURE_NO_WARNINGS", "JSMN_STATIC", "BX_CONFIG_DEBUG=1", "ENTRY_CONFIG_IMPLEMENT_MAIN=1" } --"_USRDLL",
+    flags { "NoNativeWChar", "NoEditAndContinue", "StaticRuntime", "NoExceptions" }
+
+    local buildpath = _bigfxpath .. "/build"
+    configuration "windows"
+        setBigfxTarget( "Debug", "x64", buildpath )
+        setBigfxTarget( "Release", "x64", buildpath )
+
+    configuration "macosx"
+        setBigfxTarget( "Debug", "native", buildpath )
+        setBigfxTarget( "Release", "native", buildpath )
+
+    configuration "Debug"
+        defines { "_DEBUG" }
+        flags { "Symbols" }
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize", "Symbols" }
+
+    configuration "macosx"
+        buildoptions { "-std=c++11" } --, "-stdlib=libc++" }
+end
+
+function createBgfxProject(_bgfxpath)
 	project "bgfx" 
+
 		kind "StaticLib"
 		language "C++"
 
-        local bgfxpath = path.getabsolute("../3rdparty")
-        files { bgfxpath .. "**.h", bgfxpath .. "*.cpp", bgfxpath .. "*.sc" }
+        files { _bgfxpath .. "**.h", _bgfxpath .. "*.cpp", _bgfxpath .. "*.sc" }
+end
 
-	---------------------------------------------------------
-	project "bigfx" 
-		kind "StaticLib"
-		language "C++"
+function createBigfxSolution()
+	solution "bigfx"
+        configurations { "Debug", "Release" }
 
-        local projpath = path.getabsolute("..")
-        files { projpath .. "/src/**.h", projpath .. "/src/**.cpp" }
+        platforms { "x64", "native" }
 
-        local thirdpartypath = projpath .. "/3rdparty"
-        local bxpath = thirdpartypath .. "/bx"
-		includedirs { 
-                    --PROJ_DIR .. "/3rdparty/SDL2-2.0.16/include", 
-                    --PROJ_DIR .. "/3rdparty/glew-2.1.0/include", 
-                    thirdpartypath .. "/zlib-1.2.11", 
-                    thirdpartypath .. "/jsmn", 
-                    --PROJ_DIR .. "/3rdparty/imgui",
-                    bxpath .. "/include", 
-                    bxpath .. "/include/compat/msvc",
-                    thirdpartypath }
+        local bigfxpath = path.getabsolute("..")
+        local bgfxpath = bigfxpath .. "/3rdparty/bgfx"
+        createBgfxProject(bgfxpath)
+        createBigfxProject(bigfxpath)
+end
 
-        defines { "_CRT_SECURE_NO_WARNINGS", "JSMN_STATIC", "BX_CONFIG_DEBUG=1", "ENTRY_CONFIG_IMPLEMENT_MAIN=1" } --"_USRDLL",		
-        flags { "NoNativeWChar", "NoEditAndContinue", "StaticRuntime", "NoExceptions" }
-		
-		configuration "windows"
-			SetTarget( "Debug", "x64", projpath .. "/build" )
-			SetTarget( "Release", "x64", projpath .. "/build" )
-			
-		configuration "macosx"
-			SetTarget( "Debug", "native", projpath .. "/build" )
-			SetTarget( "Release", "native", projpath .. "/build" )
-			
-		configuration "Debug"
-			defines { "_DEBUG" }
-			flags { "Symbols" }
- 
-		configuration "Release"
-			defines { "NDEBUG" }
-			flags { "Optimize", "Symbols" } 
-
-		configuration "macosx"
-            buildoptions { "-std=c++11" } --, "-stdlib=libc++" }
-
+if solution() == nil then
+    print("creating bifgx solution ...");
+    createBigfxSolution()
+end
